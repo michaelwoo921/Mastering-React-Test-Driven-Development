6acf5b2490558625f43a325b5cba6c5e5a255f93
Jung Woo <michaelwoo921@gmail.com>
2022-01-09 09:36:18 -0800

    render multiple elements in ol test

--- appointments/src/AppointmentsDayView.js
+++ appointments/src/AppointmentsDayView.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export const AppointmentsDayView = ({ appointments }) => (
-  <div id="appointmentsDayView"></div>
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map((appointment, i) => (
+        <div key={i}></div>
+      ))}
+    </ol>
+  </div>
 );
