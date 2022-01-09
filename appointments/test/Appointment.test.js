8c06bc3a927f0c26b0e6749cd0bc2c5a262459ea
Jung Woo <michaelwoo921@gmail.com>
2022-01-09 08:50:50 -0800

    backtracking

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -7,19 +7,17 @@ describe('Appointment', () => {
     const customer = { firstName: 'Ashley' };
     const component = <Appointment customer={customer} />;
     const container = document.createElement('div');
-    document.body.appendChild(container);
 
     ReactDOM.render(component, container);
-    expect(document.body.textContent).toMatch('Ashley');
+    expect(container.textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
     const customer = { firstName: 'Jordan' };
     const component = <Appointment customer={customer} />;
     const container = document.createElement('div');
-    document.body.appendChild(container);
 
     ReactDOM.render(component, container);
-    expect(document.body.textContent).toMatch('Jordan');
+    expect(container.textContent).toMatch('Jordan');
   });
 });
