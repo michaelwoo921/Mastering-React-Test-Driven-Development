ec3bcbf78738e03480514bdd6a1636db17d3a245
Jung Woo <michaelwoo921@gmail.com>
2022-01-09 08:41:37 -0800

    another failing test

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -12,4 +12,14 @@ describe('Appointment', () => {
     ReactDOM.render(component, container);
     expect(document.body.textContent).toMatch('Ashley');
   });
+
+  it('renders another customer first name', () => {
+    const customer = { firstName: 'Jordan' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
+    ReactDOM.render(component, container);
+    expect(document.body.textContent).toMatch('Jordan');
+  });
 });
